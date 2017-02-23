4a77921b5360b3f5186dacb09704e0d454255c12
Aqudei <yodi_cute07@yahoo.com>
2017-02-24 07:45:27 +0800

    Add project files.

---
+++ NAirQMon/LoginForm.vb
@@ -0,0 +1,7 @@
+﻿Public Class LoginForm
+    Private Sub Login_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If UsernameTextBox.Text = "1" And PasswordTextBox.Text = "1" Then
+            Close()
+        End If
+    End Sub
+End Class
---
+++ NAirQMon/MainForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ShowLogin()
+    End Sub
+
+    Sub ShowLogin()
+        Dim loginForm = New LoginForm
+
+        BeginInvoke(Sub(arg)
+                        loginForm.ShowDialog(arg)
+                    End Sub, Me)
+    End Sub
+End Class
