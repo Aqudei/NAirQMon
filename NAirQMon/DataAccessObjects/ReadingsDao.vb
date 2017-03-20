cc50ed69a3103052670384b936d5b6c1cf24c7e4
Aqudei <yodi_cute07@yahoo.com>
2017-03-20 21:23:27 +0800

    fixed co2 level

--- NAirQMon/DataAccessObjects/ReadingsDao.vb
+++ NAirQMon/DataAccessObjects/ReadingsDao.vb
@@ -3,7 +3,7 @@
         Try
             Using ctx As New AirQContext
                 Dim latest = ctx.SensorDataItems.Where(Function(r) r.Place = place And r.Altitude = altitude).OrderByDescending(Function(r) r.TimeRead).FirstOrDefault
-                If latest Is Nothing Then
+                If latest IsNot Nothing Then
                     Return latest.CarbonMonoxideLevel
                 Else
                     Return 0
